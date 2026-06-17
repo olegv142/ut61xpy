a42944c640b0db8af1e28259615ca155c1225bef
Oleg Volkov <olegv142@gmail.com>
2026-06-17 20:51:26 +0300

    4th central moment calculations (kurtosis)

--- ut61stat.py
+++ ut61stat.py
@@ -1,7 +1,7 @@
 import math
 from collections import namedtuple
 
-Stat = namedtuple('Stat', ('aver', 'std_dev', 'cm3'))
+Stat = namedtuple('Stat', ('aver', 'std_dev', 'cm3', 'cm4'))
 
 class StatCollector:
     """Measured values sequence stat collector"""
@@ -9,7 +9,7 @@ class StatCollector:
 
     def __init__(self):
         self.total_samples = self.total_valid = 0
-        self.total_sum = self.total_sum2 = self.total_sum3 = 0
+        self.total_sum = self.total_sum2 = self.total_sum3 = self.total_sum4 = 0
         self.total_min = float('inf')
         self.total_max = -self.total_min
         self.median_sum, self.median_cnt = 0, 0
@@ -19,10 +19,12 @@ class StatCollector:
         self.total_samples += 1
         if math.isnan(val):
             return
+        v2 = val * val
         self.total_valid += 1
         self.total_sum   += val
-        self.total_sum2  += val * val
-        self.total_sum3  += val * val * val
+        self.total_sum2  += v2
+        self.total_sum3  += v2 * val
+        self.total_sum4  += v2 * v2
         self.total_min    = min(self.total_min, val)
         self.total_max    = max(self.total_max, val)
         self.median_buff.append(val)
@@ -46,7 +48,8 @@ class StatCollector:
         sigma_2 = aver2 - aver_2
         sigma   = math.sqrt(sigma_2) if sigma_2 > 0 else 0
         cm3     = self.total_sum3 / self.total_valid - aver * (aver_2 + 3 * sigma_2)
-        return Stat(aver, sigma, cm3)
+        cm4     = self.total_sum4 / self.total_valid - aver * (aver * (aver_2 + 6 * sigma_2) + 4 * cm3)
+        return Stat(aver, sigma, cm3, cm4)
 
     def print(self):
         print('total samples  : %d' % self.total_samples)
@@ -61,22 +64,24 @@ class StatCollector:
             if st.aver:
                 print('rel. dev. [%%]  : %.2f' % (100 * st.std_dev / abs(st.aver)))
             if st.std_dev:
-                print('skewness  [%%]  : %.2f' % (100 * st.cm3 / (st.std_dev ** 3)))
+                print('skewness       : %.2f' % (st.cm3 / (st.std_dev ** 3)))
+                print('kurtosis exess : %.2f' % (st.cm4 / (st.std_dev ** 4) - 3))
 
 if __name__ == '__main__':
-    import random
+    import numpy
     from scipy import stats
 
     vals = []
     stat = StatCollector()
     for _ in range(10000):
-        v = random.random()
+        v = numpy.random.normal()
         vals.append(v)
         stat.account(v)
     
     st = stat.get_stat()
     print(st)
-    max_err = 1e-14
-    m2, m3 = stats.moment(vals, 2), stats.moment(vals, 3)
+    max_err = 1e-13
+    m2, m3, m4 = stats.moment(vals, 2), stats.moment(vals, 3), stats.moment(vals, 4)
     assert abs(m2-st.std_dev*st.std_dev) < max_err
     assert abs(m3-st.cm3) < max_err
+    assert abs(m4-st.cm4) < max_err
